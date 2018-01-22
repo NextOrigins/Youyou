a380182b952dd53ec32567d22646005a76738449
Proxy <proxy@ProxydeiMac.lan>
2018-01-22 23:06:51 +0800

    18_1_22
    +绘制AnswersDetail布局

--- app/src/main/java/com/neworld/youyou/view/mview/parents/AnswerDetail.kt
+++ app/src/main/java/com/neworld/youyou/view/mview/parents/AnswerDetail.kt
@@ -1,13 +1,32 @@
 package com.neworld.youyou.view.mview.parents
 
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.webkit.WebView
 import com.neworld.youyou.R
-import com.neworld.youyou.add.base.Activity
+import com.neworld.youyou.add.base.Fragment
+import com.neworld.youyou.utils.notNullSingleValue
 
 /**
  * @author by user on 2018/1/22.
  */
-class AnswerDetail : Activity() {
+class AnswerDetail : Fragment() {
+
+	private var recycle by notNullSingleValue<RecyclerView>()
+	private var web by notNullSingleValue<WebView>()
 	
 	override fun getContentLayoutId()
 		= R.layout.activity_answers_detail
+
+	override fun initWidget(root: View) {
+		recycle = root.findViewById<RecyclerView>(R.id._recycle).apply {
+			// TODO : waiting for service
+		}
+
+		web = root.findViewById<WebView>(R.id._web).apply {
+			// TODO : waiting for url
+		}
+
+		// item id : R.layout.item_answers_detail.xml
+	}
 }
